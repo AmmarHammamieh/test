daf8a3d5e6c888836c6470b38ff540cf31d3efb1
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-25 18:28:47 +0300

    test17

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
