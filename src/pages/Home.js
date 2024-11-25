8fb633112995752dcdc4dd124751b1b99ee149e1
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-25 14:25:28 +0300

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
-  document.head.innerHTML(`      <title>About Us</title>
-            <meta name="description" content="Learn more about us on this page!" />
-            <meta property="og:title" content="About Us" />
-            <meta property="og:description" content="Learn more about our company and team" />
-            <meta property="og:image" content="https://via.placeholder.com/150" />
-            <meta property="og:url" content="https://example.com/about" />`)
+  console.log(new Date())
   return (
     <div>
         Home
