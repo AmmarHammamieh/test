cae8284892845031085971d870e47b5549199e2c
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-25 18:03:09 +0300

    tes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Test from './pages/Test';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
