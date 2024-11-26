c3d842ee0f064c6c20aa9c4915afb3e14b52f256
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-26 10:32:17 +0300

    ttt

--- src/SEO.js
+++ src/SEO.js
@@ -13,7 +13,7 @@ return (
             <meta property="og:description" content={description} />
             { /* End Facebook tags */ }
             { /* Twitter tags */ }
-            <meta name="twitter:creator" content={name} />}
+            <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content={type} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
