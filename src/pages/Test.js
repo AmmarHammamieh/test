93074a3ffcb112c769b72331073c3281466d9945
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-26 10:28:32 +0300

    testtt

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async';
+import SEO from '../SEO';
 
 const Test = () => {
   return (
     <div>
-         <Helmet>
-            <title>About Us</title>
-            <meta name="description" content="Learn more about us on this page!" />
-            <meta property="og:title" content="About Us" />
-            <meta property="og:description" content="Learn more about our company and team" />
-            <meta property="og:image" content="https://via.placeholder.com/150" />
-            <meta property="og:url" content="https://example.com/about" />
-        </Helmet>
+        <SEO
+        title="Learning React Helmet!"
+        description="Beginner friendly page for learning React Helmet."
+        name="Company name"
+        type="article" />
         page
     </div>
   )
