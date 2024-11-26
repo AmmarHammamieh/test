923e78603162b52d717269658e2b7210081bc560
Ammar.Hammamieh <ammar.h@atcsoultion.co>
2024-11-26 10:20:59 +0300

    test7777

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Home = () => {
+  document.head.innerHTML(`      <title>About Us</title>
+            <meta name="description" content="Learn more about us on this page!" />
+            <meta property="og:title" content="About Us" />
+            <meta property="og:description" content="Learn more about our company and team" />
+            <meta property="og:image" content="https://via.placeholder.com/150" />
+            <meta property="og:url" content="https://example.com/about" />`)
   return (
     <div>
         Home
